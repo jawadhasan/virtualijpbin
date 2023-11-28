80942b6e7c2ff09293275dce9d042c864347fc77
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-11-28 12:47:49 +0100

    inidividual app strategy

---
+++ publish/scriptApp.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+echo "executing BASH...."
+
+PORT_NO=502
+
+for i in {1..50}
+do
+   
+   echo "sudo ./VirtualIJP ${PORT_NO}"
+   sudo ./VirtualIJP ${PORT_NO}   
+
+   ((PORT_NO++))
+done
+
+echo "Finishe executing BASH...."
