e58a9fe6f35990fb3eacdd85e3b2e00c549b621b
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-11-28 12:54:57 +0100

    script updated

--- publish/scriptApp.sh
+++ publish/scriptApp.sh
@@ -7,7 +7,8 @@ for i in {1..50}
 do
    
    echo "sudo ./VirtualIJP ${PORT_NO}"
-   sudo ./VirtualIJP ${PORT_NO}   
+   #sudo ./VirtualIJP ${PORT_NO}   
+   sudo nohup ./VirtualIJP ${PORT_NO} > /dev/null 2>&1&
 
    ((PORT_NO++))
 done
