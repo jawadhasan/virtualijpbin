25766674357ea4fdf7ae1e8df36625285b87168c
Jawad Shani <jawad.shani@hitachi-eu.com>
2024-01-21 12:30:22 +0100

    shell script

---
+++ publish/script.sh
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/bash
+echo "executing BASH...."
+
+PORT_NO=8181
+
+
+PUBLIC_IPADDR=$(hostname -I) #PUBLIC_IPADDR="10.50.1.202"
+echo "SERVER PUBLIC-IP = ${PUBLIC_IPADDR}"
+
+for i in {1..1}
+do
+   echo "Setup PREROUTING on PORT: $PORT_NO "
+   sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport $PORT_NO -j DNAT --to ${PUBLIC_IPADDR}:$PORT_NO
+
+   echo "Setup FORWARD on PORT: $PORT_NO "
+   sudo iptables -A FORWARD -p tcp -d ${PUBLIC_IPADDR} --dport $PORT_NO -j ACCEPT
+
+   ((PORT_NO++))
+done
+
+echo "Finishe executing BASH...."
